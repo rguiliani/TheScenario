b6466b51848782173ada986f847c9ebdfddb3c98
Rossi Guiliani <rossi@guiliani.me>
2024-01-04 21:08:25 -0700

    clearer text in non-dark mode

--- fe/app/components/Row.tsx
+++ fe/app/components/Row.tsx
@@ -6,7 +6,7 @@ import { faTrash, faPencil } from '@fortawesome/free-solid-svg-icons'
 export function Row(props: { shade: number, data: {_id: string, data?: string }}) {
   const ctx = useContext(DataContext)
   const { data, shade } = props
-  const shadeColor = shade ? 'bg-blue-100 text-slate-600' : 'bg-slate-800'
+  const shadeColor = shade ? 'bg-blue-100 text-slate-600' : 'bg-slate-800 text-white'
   return (
     <div className={`grid grid-cols-12 gap-4 ${shadeColor}`}>
       <div className='col-span-8 p-4'>{data.data}</div>
